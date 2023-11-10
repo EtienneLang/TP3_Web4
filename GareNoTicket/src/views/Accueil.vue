57489a358c2abfa6c1c1fdc49a74532f7d7accd5
langloise <etiennelanglois007@gmail.com>
2023-11-10 17:39:55 -0500

    Début de l'accueil et du login

---
+++ GareNoTicket/src/views/Accueil.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="d-flex flex-column justify-content-center align-items-center">
+            <div class="d-flex flex-column justify-content-center align-items-center p-5 mt-5">
+                <h1>
+                    <i>Gare<strong>NoTicket</strong></i>
+                </h1>
+                <p>Le premier site d'aide au stationnements pour les étudiants du cégep.</p>
+            </div>
+            <div class="d-flex flex-column justify-content-center align-items-center">
+                <p class="fs-3">Pour commencer</p>
+                <div class="btn btn-primary">S'inscrire</div>
+                <p>ou</p>
+                <div class="btn btn-primary">
+                    <RouterLink style="text-decoration: none; color: inherit" to="/login">
+                        Se connecter
+                    </RouterLink>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Accueil',
+}
+</script>
+
+<style>
+.lien {
+    color: white;
+    text-decoration: none;
+}
+/* Add any custom styles for your home page here */
+</style>
---
+++ GareNoTicket/src/views/Connection.vue
@@ -0,0 +1,40 @@
+
+<template>
+    <div>
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+            <label>
+                Email:
+                <input type="email" v-model="email" required>
+            </label>
+            <label>
+                Password:
+                <input type="password" v-model="password" required>
+            </label>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        login() {
+            // Perform login logic here, e.g. send request to server
+            console.log(`Logging in with email ${this.email} and password ${this.password}`);
+        }
+    }
+}
+</script>
+
+<style>
+/* Component styles go here */
+</style>
+
