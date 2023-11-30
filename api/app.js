5427c00b9da27bc6ac8c1596f42963c0189057b4
langloise <etiennelanglois007@gmail.com>
2023-11-30 15:41:31 -0500

    Rewind du dernier push

--- api/app.js
+++ api/app.js
@@ -39,6 +39,11 @@ app.use(seed);
 
 mongoose
   .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('Node.js est à l\'écoute sur le port %s ', PORT);
+    });
+  })
   .catch(err => console.log(err));
 
 module.exports = app;
